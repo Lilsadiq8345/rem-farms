6703afc3227ececa2572f97e7bf04ae0c1b55373
Lilsadiq8345 <abubakarsa242@gmail.com>
2024-12-26 16:08:31 +0100

    commit

--- src/components/investor/ui/Navbar.js
+++ src/components/investor/ui/Navbar.js
@@ -129,12 +129,12 @@ const NavBar = () => {
             </button>
             {isDropdownOpen && (
               <div className="absolute right-0 mt-2 w-40 bg-white border border-gray-200 shadow-lg rounded-lg">
-                <Link to="/uk" className="flex items-center px-4 py-2 hover:bg-gray-100"
+                <Link to="/home" className="flex items-center px-4 py-2 hover:bg-gray-100"
                   onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
                   <img src="/uk.png" alt="UK Flag" className="w-5 h-5 mr-2" />
                   United Kingdom
                 </Link>
-                <Link to="/nigeria" className="flex items-center px-4 py-2 hover:bg-gray-100"
+                <Link to="/home" className="flex items-center px-4 py-2 hover:bg-gray-100"
                   onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
                   <img src="/nigeria.png" alt="Nigeria Flag" className="w-5 h-5 mr-2" />
                   Nigeria
@@ -205,12 +205,12 @@ const NavBar = () => {
             </button>
             {isDropdownOpen && (
               <div className="absolute right-0 mt-2 w-40 bg-white border border-gray-200 shadow-lg rounded-lg">
-                <Link to="/uk" className="flex items-center px-4 py-2 hover:bg-gray-100"
+                <Link to="/home" className="flex items-center px-4 py-2 hover:bg-gray-100"
                   onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
                   <img src="/uk.png" alt="UK Flag" className="w-5 h-5 mr-2" />
                   United Kingdom
                 </Link>
-                <Link to="/nigeria" className="flex items-center px-4 py-2 hover:bg-gray-100"
+                <Link to="/home" className="flex items-center px-4 py-2 hover:bg-gray-100"
                   onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
                   <img src="/nigeria.png" alt="Nigeria Flag" className="w-5 h-5 mr-2" />
                   Nigeria
